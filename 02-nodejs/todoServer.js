0cc0855ae113839b8b1cbb361c57fde891f39232
Bhagavan Marpadaga <bhagavan.m@lsq.net>
2023-07-29 18:52:54 +0530

    passed test cases for todo server

--- 02-nodejs/todoServer.js
+++ 02-nodejs/todoServer.js
@@ -39,11 +39,93 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-app.use(bodyParser.json());
-
-module.exports = app;
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  
+  const app = express();
+  const { uuid } = require('uuidv4');
+  
+  app.use(bodyParser.json());
+  
+  let todos=[]
+  
+  function createTodo(req,res){
+  
+    const id=uuid()
+    let body=req.body
+    body={...body,id:id}
+    todos.push(body)
+  
+    // console.log("body is ",body)
+    res.status(201).send({id:id})
+  
+  }
+  function getAllTodos(req,res){
+  
+    return res.status(200).send(todos)
+  
+  }
+  function updateTodo(req,res){
+  
+    // console.log(req.params)
+    if(!req.params.id){
+      return res.status(400).send("Invalid request id is required")
+    }
+    const index=todos.findIndex((todo)=> todo.id=req.params.id)
+    // console.log("todo to update ",index)
+  
+    if(index!=0&&(!index|| index <0)){
+      return res.status(404).send("No todo found with the mathcing id")
+      
+    }
+    todos[index]={...todos[index],...req.body,id:req.params.id}
+  
+    // console.log("updated todos are ",todos)
+    return res.status(200).send("updated successfully")
+    
+  
+  }
+  
+  function getTodo(req,res){
+    const index=todos.findIndex(todo=>todo.id===req.params.id)
+    if(index!=0&&(!index|| index <0)){
+      return res.status(404).send("No todo found with the matching id")
+    }
+    else{
+      return res.status(200).send(todos[index])
+    }
+  }
+  function deleteTodo(req,res){
+    const index=todos.findIndex((todo)=>todo.id===req.params.id)
+    // console.log("index is ",index)
+    if(index!=0 && (!index|| index <0)){
+      return res.status(404).send("Not Found")
+    }
+    todos=todos.filter(todo=>todo.id!=req.params.id)
+    // console.log("updated items after deleting ",todos)
+    return res.status(200).send("Item found and deleted")
+  }
+  
+  app.post('/todos',createTodo)
+  app.get('/todos',getAllTodos)
+  app.put('/todos/:id',updateTodo)
+  app.get('/todos/:id',getTodo)
+  app.delete('/todos/:id',deleteTodo)
+  app.get('/',(req,res)=>{
+    res.send("cool")
+  })
+  app.all("*",(req,res)=>{
+    return res.status(404).send("Invalid path mentioned")
+  })
+  
+  
+  app.listen(4000,(err)=>{
+    if(err){
+      // console.log("failed to start server");
+      return
+    }
+    // console.log("up and running")
+  
+  })
+  
+  module.exports = app;
